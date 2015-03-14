b3770f5af515b1a697137db43f9637f84f03b01e
shuo li <shuoli84@gmail.com>
2015-03-14 22:28:19 +0800

    Add static root config

--- fitting/settings.py
+++ fitting/settings.py
@@ -84,6 +84,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'publish/static')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
