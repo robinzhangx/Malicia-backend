cde633a3a794383ab8525338b510a83a7f391eb3
shuo li <shuoli84@gmail.com>
2015-03-17 23:51:03 +0800

    Add pagination on fitting, now the fitting returns global fittings, not filtered by fitting score

--- fitting/settings.py
+++ fitting/settings.py
@@ -110,7 +110,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'ft_fitting.pagination.LinkHeaderPagination',
+    'PAGE_SIZE': 20
 }
 
 # Authentication backend
