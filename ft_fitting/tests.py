7056d97ba51d24f5d499921162f2436b6ebc16bb
shuo li <shuoli84@gmail.com>
2015-03-18 23:09:41 +0800

    Add fitting count api

--- ft_fitting/tests.py
+++ ft_fitting/tests.py
@@ -1,3 +1,18 @@
+import json
 from django.test import TestCase
+from ft_accounts.models import User
+from ft_fitting.models import Fitting
 
-# Create your tests here.
+
+class FittingTest(TestCase):
+    def test_fitting_count(self):
+        user = User(nickname='test')
+        user.save()
+        f = Fitting()
+        f.user = user
+        f.save()
+
+        response = self.client.get("/api/fittings/count/")
+        self.assertEqual(response.status_code, 200)
+        obj = json.loads(response.content)
+        self.assertEqual(obj['count'], 1)
--- ft_fitting/views.py
+++ ft_fitting/views.py
@@ -1,3 +1,5 @@
+from rest_framework.decorators import list_route
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from ft_fitting.models import Fitting
 from ft_fitting.serializers import FittingSerializer
@@ -11,3 +13,7 @@ class FittingViewSet(ModelViewSet):
     def get_queryset(self):
         qs = super(FittingViewSet, self).get_queryset()
         return qs
+
+    @list_route(methods=['get'])
+    def count(self, request):
+        return Response({'count': self.get_queryset().count()})
