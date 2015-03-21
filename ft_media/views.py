7956bf512efefe4f2eaed1f6366b2b58cdc0e592
shuo li <shuoli84@gmail.com>
2015-03-21 13:45:10 +0800

    Return path as /media/images/xxxxx

--- ft_media/views.py
+++ ft_media/views.py
@@ -1,4 +1,5 @@
 import os
+from django.conf import settings
 from rest_framework.parsers import FileUploadParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -12,10 +13,12 @@ class UploadApiView(APIView):
 
     def put(self, request, filename):
         file_obj = request.data['file']
+        url = os.path.join('images', filename)
         if not default_storage.exists(os.path.join('images', filename)):
-            path = default_storage.save(os.path.join('images', filename), file_obj)
+            default_storage.save(os.path.join('images', filename), file_obj)
         else:
-            path = default_storage.path(os.path.join('images', filename))
-        return Response({"path": path}, status=200)
+            default_storage.path(os.path.join('images', filename))
+
+        return Response({"path": os.path.join(settings.MEDIA_URL, url)}, status=200)
 
 upload_view = UploadApiView.as_view()
